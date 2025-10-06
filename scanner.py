ac909e7f924eae7a2d252aa0e45ea50d43c0fc5b
wallbe <wallbe@iu.edu>
2025-10-06 19:59:44 -0400

    threader portion

--- scanner.py
+++ scanner.py
@@ -6,6 +6,7 @@ from concurrent.futures import ThreadPoolExecutor
 class portScanner():
     def __init__(self):
         pass
+    @staticmethod
     def portScanner(target, port):
 
         try:
@@ -15,14 +16,24 @@ class portScanner():
                 #open port
                 if result == 0:
                     print(f"Port: {port} is open")
-                #Filtered ports
+                #closed ports
                 elif result in [111,113]:
-                    print(f"Port: {port} is filtered")
-                #closed port
-                else:
                     print(f"Port: {port} is closed")
+                #filtered port
+                else:
+                    print(f"Port: {port} is filtered")
 
 
         except Exception as e:
             print(f"Exception Error: {e}")
-   
+        
+    def threader(target, thread_count=500):
+        #Will be responsible for creating multiple threads, paralle scanning
+        ports = [20,21,22,23,25,53,80,110,143,443,995,3389]
+        with ThreadPoolExecutor(max_workers=thread_count) as executor:
+            for port in ports:
+                executor.submit(portScanner.portScanner, target, port)
+            executor.shutdown(wait=True)
+            print("Port Scanning Completed")
+
+    
